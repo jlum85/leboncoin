30681fb9ed6e6c39e29f1d569d49a835d4cb313c
jlum85 <jlum@wanadoo.fr>
2019-11-17 23:16:12 +0100

    route offer

--- src/containers/Offer.js
+++ src/containers/Offer.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import "../App.css";
+import "./Offer.css";
 import axios from "axios";
 
 const Offer = () => {
   const obj = useParams();
-
   const [offer, setOffer] = useState({});
+
+  console.log(offer);
 
   const fetchData = async () => {
     const response = await axios.get(
@@ -19,31 +21,58 @@ const Offer = () => {
     fetchData();
   }, []);
 
+  const getFormattedDate = () => {
+    const dateCreated = new Date(offer.created);
+    return (
+      dateCreated.toLocaleDateString() +
+      " à " +
+      dateCreated.toLocaleTimeString()
+    );
+  };
+
   return (
-    <>
-      <h1>Hello {obj.id}</h1>
-      <div>
-        {offer.pictures && offer.pictures.length > 0 ? (
-          <img className="offerImg" src={offer.pictures[0]} alt="Offre"></img>
-        ) : (
-          <h3>pas image </h3>
-        )}
+    <section className="offer">
+      <div className="container">
+        <article>
+          <div className="offer-content">
+            <div className="offer-detail box-shadow">
+              <div className="bg-silver center">
+                {offer.pictures && offer.pictures.length > 0 ? (
+                  <img
+                    className="offerImg"
+                    src={offer.pictures[0]}
+                    alt="Offre"
+                  ></img>
+                ) : (
+                  <h3>No image </h3>
+                )}
+              </div>
+              <div className="offer-infos">
+                <div className="offer-inf">
+                  <div className="offer-title">{offer.title}</div>
+                  <div className="offer-price">{offer.price}</div>
+                </div>
+                <div className="offer-created">{getFormattedDate()}</div>
+              </div>
+            </div>
+            <div className="offer-description">
+              <div className="offer-title">Description</div>
+              <div>{offer.description}</div>
+            </div>
+          </div>
+        </article>
+        <aside className="userInfos box-shadow">
+          <div className="user-detail">
+            {offer.creator ? offer.creator.account.username : ""}
+            <div className="user-count"> 17 annonces en lignes </div>
+          </div>
+          <div className="puchaseFlex">
+            <button className="puchaseBtn">Acheter</button>
+          </div>
+        </aside>
       </div>
-    </>
+    </section>
   );
 };
 
 export default Offer;
-// return (
-//   <section>
-//     <div className="container">
-//       <article>
-//         <div>
-//           <img src="" alt=""></img>
-//           <div className="infos"></div>
-//         </div>
-//         <div className="description"></div>
-//       </article>
-//     </div>
-//   </section>
-//);
